4875ae3e822af97c1cecf8bcf59a6fddcd3b0df9
egor <esn@anadeainc.com>
2020-12-20 14:44:19 +0200

    split MeetupPage into components

--- 02-components/02-MeetupPageComponent/MeetupAgendaItem.js
+++ 02-components/02-MeetupPageComponent/MeetupAgendaItem.js
@@ -1,23 +1,33 @@
+import { agendaItemTitles, agendaItemIcons} from './data.js'
+
 export const MeetupAgendaItem = {
   name: 'MeetupAgendaItem',
 
   template: `<div class="meetup-agenda__item">
       <div class="meetup-agenda__item-col">
-        <img class="icon" alt="icon" src="/assets/icons/icon-cal-sm.svg" />
+        <img class="icon" alt="icon" :src="\`/assets/icons/icon-\${ iconType }.svg\`" />
       </div>
-      <div class="meetup-agenda__item-col">00:00 - 00:00</div>
+      <div class="meetup-agenda__item-col">{{ agendaItem.startsAt }} - {{ agendaItem.endsAt }}</div>
       <div class="meetup-agenda__item-col">
-        <h5 class="meetup-agenda__title">Заголовок</h5>
-        <p>
-          <span>Докладчик</span>
+        <h5 class="meetup-agenda__title">{{ agendaItem.title ? agendaItem.title : defaultTitle }}</h5>
+        <p v-if="agendaItem.type === 'talk'">
+          <span>{{ agendaItem.speaker }}</span>
           <span class="meetup-agenda__dot"></span>
-          <span class="meetup-agenda__lang">ru</span>
+          <span class="meetup-agenda__lang">{{ agendaItem.language }}</span>
         </p>
-        <p>Описание</p>
+        <p v-if="agendaItem.description">{{ agendaItem.description }}</p>
       </div>
     </div>`,
 
-  // props
+  props: {
+    agendaItem: {
+      type: Object,
+      required: true
+    }
+  },
 
-  // Возможно, тут потребуется computed
-};
+  computed: {
+    iconType() { return agendaItemIcons[this.agendaItem.type] },
+    defaultTitle() { return agendaItemTitles[this.agendaItem.type] }
+  }
+}
--- 02-components/02-MeetupPageComponent/MeetupView.js
+++ 02-components/02-MeetupPageComponent/MeetupView.js
@@ -1,32 +1,47 @@
-// import { MeetupCover } from './MeetupCover.js';
-// import { MeetupDescription } from './MeetupDescription.js';
-// import { MeetupAgenda } from './MeetupAgenda.js';
-// import { MeetupInfo } from './MeetupInfo.js';
-// import { getMeetupCoverLink } from './data.js';
+import { MeetupCover } from './MeetupCover.js'
+import { MeetupDescription } from './MeetupDescription.js'
+import { MeetupAgenda } from './MeetupAgenda.js'
+import { MeetupInfo } from './MeetupInfo.js'
+import { getMeetupCoverLink } from './data.js'
 
 export const MeetupView = {
   name: 'MeetupView',
 
   template: `
     <div>
-      <!-- meetup cover -->
+      <meetup-cover :title="meetup.title" :link="link" />
       <div class="container">
         <div class="meetup">
           <div class="meetup__content">
             <h3>Описание</h3>
-            <!-- meetup description -->
+            <meetup-description v-if="meetup.description" :description="meetup.description" />
 
             <h3>Программа</h3>
-            <!-- meetup agenda -->
+            <meetup-agenda :agenda="meetup.agenda" />
           </div>
           <div class="meetup__aside">
-            <!-- meetup info -->
+            <meetup-info :organizer="meetup.organizer" :place="meetup.place" :date="dateTime" />
           </div>
         </div>
       </div>
     </div>`,
 
-  // components
+  components: {
+    MeetupCover,
+    MeetupDescription,
+    MeetupAgenda,
+    MeetupInfo
+  },
 
-  // props
-};
+  computed: {
+    link() { return getMeetupCoverLink(this.meetup) },
+    dateTime() { return new Date(this.meetup.date) }
+  },
+
+  props: {
+    meetup: {
+      type: Object,
+      required: true
+    }
+  }
+}
