c6958d559bf5fcb97eb55b46c2d18cde94a3545b
egor nosach <egor@egors-MacBook-Air.local>
2020-12-27 17:28:10 +0200

    layouts

--- 04-SPA/04-Layouts/components/AuthLayout.vue
+++ 04-SPA/04-Layouts/components/AuthLayout.vue
@@ -1,9 +1,42 @@
-<template></template>
+<template>
+  <div class="page page_onboarding">
+    <div class="container">
+      <h1 class="page__title text-center">{{ title }}</h1>
+    </div>
+  </div>  
+</template>
 
 <script>
 export default {
   name: 'AuthLayout',
+
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  }
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.page.page_onboarding {
+    max-width: 374px;
+    width: 100%;
+    margin: 0 auto;
+}
+.container {
+  max-width: 1008px;
+  width: 100%;
+  margin: 0 auto;
+}
+
+.page__title {
+  font-family: Roboto, sans-serif;
+  font-weight: 700;
+  font-size: 32px;
+  line-height: 37px;
+  color: var(--body-color);
+  margin: 0 0 32px;
+}
+</style>
--- 04-SPA/04-Layouts/components/FormLayout.vue
+++ 04-SPA/04-Layouts/components/FormLayout.vue
@@ -1,9 +1,39 @@
-<template></template>
+<template>
+  <div class="page page_meetup-forms">
+    <div class="container">
+      <h2 class="page__title">{{ title }}</h2>
+    </div>
+  </div>
+</template>
 
 <script>
 export default {
   name: 'FormLayout',
+
+  props: {
+    title: {
+      type: String,
+      required: true
+    }
+  }
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.page.page_meetup-forms {
+    padding: 70px 0;
+}
+.container {
+  max-width: 1008px;
+  width: 100%;
+  margin: 0 auto;
+}
+.page__title {
+  font-family: Roboto, sans-serif;
+  font-weight: 700;
+  font-size: 32px;
+  line-height: 37px;
+  color: var(--body-color);
+  margin: 0 0 32px;
+}
+</style>
