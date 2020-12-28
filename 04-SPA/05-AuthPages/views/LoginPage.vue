0a2f36471e2611aa2f37e9f7044637361562e926
egor nosach <egor@egors-MacBook-Air.local>
2020-12-28 11:34:16 +0200

    validation for login and register form

--- 04-SPA/05-AuthPages/views/LoginPage.vue
+++ 04-SPA/05-AuthPages/views/LoginPage.vue
@@ -1,12 +1,66 @@
 <template>
-  <form class="form"></form>
+  <form class="form" @submit.prevent="handleSubmit">
+    <div class="form-group">
+      <label class="form-label">Email</label>
+      <div class="input-group">
+        <input v-model="email" type="email" placeholder="demo@email" class="form-control" />
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="form-label">Пароль</label>
+      <div class="input-group">
+        <input v-model="password" type="password" placeholder="password" class="form-control" />
+      </div>
+    </div>
+    <div class="form__buttons">
+      <button type="submit" class="button button_primary button_block">Войти</button>
+    </div>
+    <div class="form__append">Нет аккаунта?
+      <router-link to="/register" class="link">Зарегистрируйтесь</router-link>
+    </div>
+  </form>
 </template>
 
 <script>
-// import { login } from '../data';
+import { login } from '../data';
 
 export default {
   name: 'LoginPage',
+
+  data() {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+
+  methods: {
+    hasEmptyInputs() {
+      return this.email === '' || this.password === ''
+    },
+    showAlertMessage() {
+      if(this.email === '') {
+        alert('Требуется ввести Email')
+      } else if (this.password === '') {
+        alert('Требуется ввести пароль')
+      }
+    },
+    handleSubmit() {
+      if (this.hasEmptyInputs()) {
+        this.showAlertMessage()
+      } else {
+        login(this.email, this.password)
+          .then(res => {
+            if(res.error) {
+              alert(res.message)
+            } else {
+              alert(res.fullname)
+            }
+          })
+      }
+    }
+  }
+
 };
 </script>
 
--- 04-SPA/05-AuthPages/views/RegisterPage.vue
+++ 04-SPA/05-AuthPages/views/RegisterPage.vue
@@ -1,12 +1,102 @@
 <template>
-  <form class="form"></form>
+  <form @submit.prevent="handleSubmit" class="form">
+    <div class="form-group">
+      <label class="form-label">Email</label>
+      <div class="input-group">
+        <input v-model="email" type="email" class="form-control" />
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="form-label">Имя</label>
+      <div class="input-group">
+        <input v-model="fullname" type="text" class="form-control" />
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="form-label">Пароль</label>
+      <div class="input-group">
+        <input v-model="password" type="password" class="form-control" />
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="form-label">Повтор пароля</label>
+      <div class="input-group">
+        <input v-model="confirmPassword" type="password" class="form-control" />
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="checkbox"><input v-model="isAgree" type="checkbox" /> Я согласен с условиями <span></span></label>
+    </div>
+    <div class="form__buttons">
+      <button type="submit" class="button button_primary">Зарегистрироваться</button>
+    </div>
+    <div class="form__append">Уже есть аккаунт?
+      <router-link to="/login" class="link">Войдите</router-link>
+    </div>
+  </form>
 </template>
 
 <script>
-// import { register } from '../data';
+import { register } from '../data';
 
 export default {
   name: 'RegisterPage',
+
+  data() {
+    return {
+      email: '',
+      password: '',
+      confirmPassword: '',
+      fullname: '',
+      isAgree: false
+    }
+  },
+
+  computed: {
+    isPasswordConfirm() {
+      return parseInt(this.password) === parseInt(this.confirmPassword)
+    }
+  },
+
+  methods: {
+    validateForm() {
+      return this.email === ''
+      || this.password === ''
+      || this.fullname === ''
+      || !this.isAgree
+      || !this.isPasswordConfirm
+    },
+    showAlertMessage() {
+      if (this.email === '') {
+        alert('Требуется ввести Email')
+      } else if (this.fullname === '') {
+        alert('Требуется ввести полное имя')
+      } else if (this.password === '') {
+        alert('Требуется ввести пароль')
+      } else if (this.confirmPassword === '') {
+        alert('Требуется ввести повтор пароля')
+      } else if (!this.isPasswordConfirm) {
+        alert('Пароли не совпадают')
+      } else if (!this.isAgree) {
+        alert('Требуется согласиться с условиями')
+      }
+    },
+    handleSubmit() {
+      if(this.validateForm()) {
+        this.showAlertMessage()
+      } else {
+        register(this.email, this.fullname, this.password)
+          .then(res => {
+            if(res.error) {
+              alert(res.message)
+            } else {
+              alert(res.id)
+            }
+          })
+          .catch(e => {console.log(e)})
+      }
+    }
+  }
 };
 </script>
 
