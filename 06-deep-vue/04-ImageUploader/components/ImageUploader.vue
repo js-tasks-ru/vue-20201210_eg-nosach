a3e2920e95419a576f1e55792295f4670a3a4419
egor nosach <egor@egors-MacBook-Air.local>
2021-01-10 02:28:56 +0200

    ImageUploader

--- 06-deep-vue/04-ImageUploader/components/ImageUploader.vue
+++ 06-deep-vue/04-ImageUploader/components/ImageUploader.vue
@@ -1,24 +1,75 @@
 <template>
   <div class="image-uploader">
     <label
-      class="image-uploader__preview image-uploader__preview-loading"
-      :style="`--bg-image: url('https://course-vue.javascript.ru/api/images/1')`"
+      class="image-uploader__preview"
+      :class="{'image-uploader__preview-loading': isLoading}"
+      :style="imageUrl"
+      @click="handleClick"
     >
-      <span>Удалить изображение</span>
+      <span>{{ imageText }}</span>
       <input
         type="file"
         accept="image/*"
         class="form-control-file"
+        :disabled="isLoading"
+        @change="handleChange"
+        ref="input"
       />
     </label>
   </div>
 </template>
 
 <script>
-// import { ImageService } from '../image-service';
+import { ImageService } from '../image-service';
 
 export default {
   name: 'ImageUploader',
+
+  data() {
+    return {
+      isLoading: false
+    }
+  },
+
+  props: {
+    imageId: {
+      default: null
+    }
+  },
+
+  model: {
+    prop: 'imageId',
+    event: 'change'
+  },
+
+  computed: {
+    imageText() {
+      if (this.isLoading) { return 'Загрузка...'}
+
+      return this.imageId ? 'Удалить изображение' : 'Загрузить изображение'
+    },
+    imageUrl() {
+      let url = ImageService.getImageURL(this.imageId)
+      return url ? { '--bg-image': `url('${url}')` } : {}
+    }
+  },
+
+  methods: {
+    handleChange(e) {
+      this.isLoading = true
+      ImageService.uploadImage(...e.target.files).then(res => {
+        this.isLoading = false
+        this.$emit('change', res.id)
+      })
+    },
+    handleClick(e) {
+      if (this.imageId) {
+        this.$emit('change', null)
+        this.$refs.input.value = null
+        e.preventDefault()
+      }
+    }
+  }
 };
 </script>
 
