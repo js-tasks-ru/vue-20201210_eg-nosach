cf4d2750eb1e5819c707e3ac692f50c67a1bf05b
egor nosach <egor@egors-MacBook-Air.local>
2021-01-12 22:15:41 +0200

    NodesPagination

--- 06-deep-vue/07-NodesPagination/components/NodesPagination.vue
+++ 06-deep-vue/07-NodesPagination/components/NodesPagination.vue
@@ -1,6 +1,28 @@
 <script>
 export default {
   name: 'NodesPagination',
+
+  props: {
+    page: Number,
+    perPage: Number
+  },
+
+  computed: {
+    slotsPerPage() {
+      let from = this.$props.perPage * (this.$props.page - 1)
+      let to = this.$props.perPage * this.$props.page
+      let slots = this.$slots.default || []
+
+      return slots.slice(from, to)
+    }
+  },
+
+  render(h) {
+    return h(
+      'div',
+      this.slotsPerPage
+    )
+  }
 };
 </script>
 
