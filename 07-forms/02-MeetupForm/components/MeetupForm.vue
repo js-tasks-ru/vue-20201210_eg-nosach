1b69572b9a706bd4e9c0c7caa8cfad3e47586879
egor nosach <egor@egors-MacBook-Air.local>
2021-01-15 02:04:53 +0200

    MeetupForm

--- 07-forms/02-MeetupForm/components/MeetupForm.vue
+++ 07-forms/02-MeetupForm/components/MeetupForm.vue
@@ -1,34 +1,42 @@
 <template>
-  <form class="form meetup-form">
+  <form class="form meetup-form" @submit="handleSubmit">
     <div class="meetup-form__content">
       <fieldset class="form-section">
         <div class="form-group">
           <label class="form-label">Название</label>
-          <input class="form-control" />
+          <input class="form-control" v-model="meetup_.title" />
         </div>
         <div class="form-group">
           <label class="form-label">Дата</label>
-          <input class="form-control" type="date" />
+          <input class="form-control" type="date" v-model="inputDate"/>
         </div>
         <div class="form-group">
           <label class="form-label">Место</label>
-          <input class="form-control" />
+          <input class="form-control" v-model="meetup_.place" />
         </div>
         <div class="form-group">
           <label class="form-label">Описание</label>
-          <textarea class="form-control" rows="3"></textarea>
+          <textarea class="form-control" rows="3" v-model="meetup_.description"></textarea>
         </div>
         <div class="form-group">
           <label class="form-label">Изображение</label>
-          <image-uploader />
+          <image-uploader v-model="meetup_.imageId" />
         </div>
       </fieldset>
 
       <h3 class="form__section-title">Программа</h3>
-      <!-- <meetup-agenda-item-form class="mb-3" />-->
+
+      <template v-for="(agendaItem, index) in meetup_.agenda">
+        <meetup-agenda-item-form
+          class="mb-3"
+          :key="agendaItem.id"
+          :agendaItem.sync="meetup_.agenda[index]"
+          @remove="removeAgendaItem(index)"
+        />
+      </template>
 
       <div class="form-section_append">
-        <button type="button" data-test="addAgendaItem">
+        <button type="button" data-test="addAgendaItem" @click="addAgendaItem">
           + Добавить этап программы
         </button>
       </div>
@@ -40,6 +48,7 @@
           class="button button_secondary button_block"
           type="button"
           data-test="cancel"
+          @click="handleCancel"
         >
           Отмена
         </button>
@@ -48,7 +57,7 @@
           type="submit"
           data-test="submit"
         >
-          Submit
+          {{ submitText }}
         </button>
       </div>
     </div>
@@ -59,10 +68,14 @@
 import MeetupAgendaItemForm from './MeetupAgendaItemForm.vue';
 import ImageUploader from './ImageUploader';
 
-function buildAgendaItem() {
+function deepClone(v) {
+  return JSON.parse(JSON.stringify(v))
+}
+
+function buildAgendaItem(startsAt = '00:00') {
   return {
     id: Math.random(),
-    startsAt: '00:00',
+    startsAt,
     endsAt: '00:00',
     type: 'other',
     title: null,
@@ -79,6 +92,60 @@ export default {
     ImageUploader,
     MeetupAgendaItemForm,
   },
+
+  props: {
+    meetup: {
+      type: Object,
+      required: true
+    },
+    submitText: {
+      type: String,
+      required: true
+    }
+  },
+
+  data() {
+    return {
+      meetup_: deepClone(this.meetup)
+    }
+  },
+
+  computed: {
+    inputDate: {
+      get() {
+        return this.dateFormat(this.meetup_.date)
+      },
+      set(v) {
+        this.meetup_.date = v
+      }
+    }
+  },
+
+  methods: {
+    dateFormat(v) {
+      let d = new Date(v)
+      let YYYY = d.getUTCFullYear()
+      let MM = (d.getUTCMonth() + 1).toString().padStart(2, '0')
+      let DD = d.getUTCDate().toString().padStart(2, '0')
+
+      return `${YYYY}-${MM}-${DD}`
+    },
+    addAgendaItem() {
+      let agenda = this.meetup_.agenda
+      let endsAt = agenda.length !== 0 ? agenda[agenda.length - 1].endsAt : undefined
+
+      agenda.push(buildAgendaItem(endsAt))
+    },
+    removeAgendaItem(index) {
+      this.meetup_.agenda.splice(index, 1)
+    },
+    handleCancel() {
+      this.$emit('cancel')
+    },
+    handleSubmit(e) {
+      this.$emit('submit', deepClone(this.meetup_))
+    }
+  }
 };
 </script>
 
