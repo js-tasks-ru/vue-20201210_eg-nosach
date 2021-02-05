f2038e26765bdfcc8cf231ce8485e7b244ba4a82
egor nosach <egor@egors-MacBook-Air.local>
2021-02-05 15:45:46 +0200

    create AppCheckbox

--- 06-deep-vue/09-AppCheckbox/components/AppCheckbox.vue
+++ 06-deep-vue/09-AppCheckbox/components/AppCheckbox.vue
@@ -1,14 +1,49 @@
 <template>
   <label class="checkbox">
-    <input type="checkbox" />
-    <slot />
+    <input
+      type="checkbox"
+      v-model="model"
+      :value="value"
+      v-bind="$attrs"
+      v-on="$listeners"
+      />
+    <slot name="default" />
     <span></span>
   </label>
 </template>
 
 <script>
+import { omit } from 'lodash'
+
 export default {
   name: 'AppCheckbox',
+
+  inheritAttrs: false,
+
+  props: {
+    checked: [Boolean, Array],
+    value: String
+  },
+
+  model: {
+    prop: "checked",
+    event: "change"
+  },
+
+  computed: {
+    model: {
+      get() {
+        return this.checked
+      },
+      set(v) {
+        this.$emit('change', v)
+      }
+    },
+    listeners() {
+      return omit(this.$listeners, ['change'])
+    }
+  }
+
 };
 </script>
 
